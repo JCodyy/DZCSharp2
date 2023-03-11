4fbb850a42f3de28c784acc2622e3cf7d6b83291
Nikita <Roux933@mail.ru>
2023-03-11 10:41:06 +0300

    Zadach15

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -1,22 +1,38 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-int number = ReadInt("Введите трехзначное число: ");
-int amount = number.ToString().Length;
-if (amount < 3 || amount > 3)
+// int number = ReadInt("Введите трехзначное число: ");
+// int amount = number.ToString().Length;
+// if (amount < 3 || amount > 3)
+// {
+//     Console.WriteLine("Вы ввели не трехзначное число");
+// }
+// else
+// {
+//     Console.WriteLine(InCenter(number));
+// }
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// int InCenter(int a)
+// {
+//     int result = ((a / 10) % 10);
+//     return result;
+// }
+
+
+int Factorial(int n)
 {
-    Console.WriteLine("Вы ввели не трехзначное число");
+    if (n == 1) return 1;
+ 
+    return n * Factorial(n - 1);
 }
-else
-{
-    Console.WriteLine(InCenter(number));
-}
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
-int InCenter(int a)
-{
-    int result = ((a / 10) % 10);
-    return result;
-}
+ 
+int factorial4 = Factorial(4);  // 24
+int factorial5 = Factorial(5);  // 120
+int factorial6 = Factorial(6);  // 720
+ 
+Console.WriteLine($"Факториал числа 4 = {factorial4}");
+Console.WriteLine($"Факториал числа 5 = {factorial5}");
+Console.WriteLine($"Факториал числа 6 = {factorial6}");
--- Zadacha15/Program.cs
+++ Zadacha15/Program.cs
@@ -1,7 +1,7 @@
 ﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
 
 
-int dayNumber = ReadInt("Введите число от 1 до 7: ");
+int dayNumber = ReadInt("Введите день недели: ");
 Console.WriteLine(WorkHoliday(dayNumber));
 int ReadInt(string message)
 {
@@ -14,16 +14,16 @@ string WorkHoliday(int a)
     {
         if (a == 7 || a == 6)
         {
-            Console.Write("Под цифрой " + a + " - Выходной");
+            Console.Write(a + " Выходной");
         }
         else
         {
-            Console.Write("Под цифрой " + a + " - Рабочий");
+            Console.Write(a + " Рабочий");
         }
     }
     else
     {
         Console.Write("Вы ввели число не в пределах от 1 до 7, поэтому не возможно определить");
     }
-    return " день.";
+    return " день";
 }
